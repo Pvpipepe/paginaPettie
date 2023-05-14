c4945a4d4d04538493e84c125892e81d250c60c7
Pvpipepe <jose.manuel3010@hotmail.com>
2023-05-14 14:12:30 -0400

    Se agrego "Sobre Nosotros" y funciones con Jquery

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+
+$("#clientesSemanales").hide();
+
+$("#patitas").click(function(){
+    $("#clientesSemanales").show();
+});
+
+$("#patitasOcultar").click(function(){
+    $("#clientesSemanales").hide();
+});
